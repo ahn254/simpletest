2fbf2a8aaca323e7387c4f1b123008be3ba6e22a
Alexey Ayupov <alexey.ayupov@akvelon.com>
2023-07-24 13:46:54 +0200

    Update json and script

--- scripts/build-dynamic-matrix.ps1
+++ scripts/build-dynamic-matrix.ps1
@@ -2,38 +2,26 @@ param(
     [String] [Parameter (Mandatory)] $agentSpec
 )
 
-$AllowedCanaryTests =  Get-Content -Path ./_canary.json -Raw | ConvertFrom-Json -Depth 9
+$AllowedCanaryTests =  Get-Content -Path ./_canary.json -Raw | ConvertFrom-Json -Depth 9 `
+                | Where-Object { ($_.labels -eq "all") -or ($_.labels -eq $agentSpec) -or `
+                ($_.labels -eq $agentSpec.Split("-")[0]) }
 $matrixArray = @()
-foreach ($test in $AllowedCanaryTests) {
-    if (-not [string]::IsNullOrEmpty($($test | Where-Object {$_.supportedOSes -match $agentSpec}).name)) {
-        if ($test.templates.Count -eq 1 ) {
-            $testPath = './.github/actions/' + $test.templates
-            $testNmae = $test.name
-        } else {
-            switch -Exact -Regex ($agentSpec)
-            {
-                "ubuntu" {$suffix = "-ubnt"}
-                "win" {$suffix = "-win"}
-                "mac" {$suffix = "-mac"}
-            }
-            $testPath = './.github/actions/' + $($test.templates | Where-Object {$_ -match $suffix})
-            $testNmae = $test.name
-        }
 
-        if ([string]::IsNullOrEmpty($test.matrix)) {
-            $matrixArray += @([pscustomobject]@{actions=$testPath;testNmae=$testNmae;parameter=$null})
-        } else {
-            foreach ($matrixItem in $test.matrix.$agentSpec) {
-                $tempNmae = $testNmae + ($matrixItem.Split(":")[-1]).replace('"','')
-                $matrixArray += @([pscustomobject]@{actions=$testPath;testNmae=$tempNmae;parameter=$matrixItem})
-            }
+foreach ($test in $AllowedCanaryTests) {
+
+    $testPath = './.github/actions/' + $test.templates
+    $testNmae = $test.name
+    if ([string]::IsNullOrEmpty($test.matrix)) {
+        $matrixArray += @([pscustomobject]@{actions=$testPath;testNmae=$testNmae;parameter=$null})
+    } else {
+        foreach ($matrixItem in $test.matrix.$agentSpec) {
+            $tempNmae = $testNmae + ($matrixItem.Split(":")[-1]).replace('"','')
+            $matrixArray += @([pscustomobject]@{actions=$testPath;testNmae=$tempNmae;parameter=$matrixItem})
         }
     }
 }
 [pscustomobject]$matrix = @{
-include = $matrixArray
+    include = $matrixArray
 }
-
-write-host $($matrix | ConvertTo-Json -Compress)
 
 "matrix=$($matrix | ConvertTo-Json -Compress)" | Out-File -Append -FilePath $env:GITHUB_OUTPUT
